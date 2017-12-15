42cc860354642529e7e3de58aae6397f927352fa
Evan Layher <evan.layher@psych.ucsb.edu>
2017-12-15 14:52:04 -0800

    1.3: Minor updates

--- backslash_script.sh
+++ backslash_script.sh
@@ -2,16 +2,38 @@
 #--------------------------------------------------------------------------------------#
 # Created: 06/22/15 By: Evan Layher (evan.layher@psych.ucsb.edu)
 # Revised: 10/21/15 By: Evan Layher # (1.2): Mac compatible plus minor alterations
+# Revised: 12/13/17 By: Evan Layher # (1.3): Minor updates
 #--------------------------------------------------------------------------------------#
 # Create template function with backslashes before $ " \ ` characters
 # Then copy and paste this function into 'create_script.sh'
 #--------------------------- DEFAULT SETTINGS ------------------------------#
-text_editors=('kwrite' 'gedit' 'open -a /Applications/TextWrangler.app' 'open' 'nano' 'emacs') # text editor commands in order of preference
+text_editors=('kwrite' 'kate' 'gedit' 'open -a /Applications/BBEdit.app' 'open') # GUI text editor commands in preference order
 script_number='1' # Default script number
+
+#------------------------- SCRIPT HELP MESSAGE -----------------------------#
+usage () { # Help message: '-h' or '--help' option
+	echo "${red}HELP MESSAGE: ${gre}${script_path}${whi}
+${ora}DESCRIPTION${whi}: Make 'create_script' function and copy into 'create_script.sh'
+     
+${ora}USAGE${whi}: Input template script and script number
+ [${ora}1${whi}] ${gre}${script_path}${ora} ${HOME}/template.sh ${red}5${whi}
+     
+${ora}OPTIONS${whi}: Can input multiple options in any order
+ ${pur}-h${whi} or ${pur}--help${whi} Display this message
+ ${pur}-nc${whi}  Prevent color printing in terminal
+ ${pur}-o${whi} or ${pur}--open${whi} Open this script
+     
+${ora}DEFAULT SETTINGS${whi}:
+ script number: ${gre}${script_number}${whi}
+     
+${ora}VERSION: ${gre}${version}${whi}
+${red}END OF HELP: ${gre}${script_path}${whi}"
+	exit_message 0
+} # usage
 
 #----------------------- GENERAL SCRIPT VARIABLES --------------------------#
 script_path="${BASH_SOURCE[0]}" # Full path of this script
-version_number='1.2'
+version='1.3'
 
 	###--- 'yes' or 'no' options (inputs do the opposite of default) ---###
 activate_colors='yes' # 'yes': Display messages in color [INPUT: '-nc']
@@ -23,8 +45,8 @@ suggest_help='no'     # 'no' : Suggest help (within script option: '-nh')
 #-------------------------------- FUNCTIONS --------------------------------#
 option_eval () { # Evaluates command line options
 	if [ "${1}" == '-h' 2>/dev/null ] || [ "${1}" == '--help' 2>/dev/null ] || \
-	[ "${1}" == '-i' 2>/dev/null ] || [ "${1}" == '-nc' 2>/dev/null ] || \
-	[ "${1}" == '-o' 2>/dev/null ] || [ "${1}" == '--open' 2>/dev/null ]; then
+	   [ "${1}" == '-i' 2>/dev/null ] || [ "${1}" == '-nc' 2>/dev/null ] || \
+	   [ "${1}" == '-o' 2>/dev/null ] || [ "${1}" == '--open' 2>/dev/null ]; then
 		activate_options "${1}"
 	elif [ -f "${1}" ]; then
 		input_file="${1}"
@@ -44,63 +66,61 @@ activate_options () { # Activate input options
 		activate_colors='no' # Do NOT display messages in color
 	elif [ "${1}" == '-o' ] || [ "${1}" == '--open' ]; then
 		open_script='yes'    # Open this script
-	else # if option is not defined here (for debugging)
+	else # if option is undefined (for debugging)
 		bad_inputs+=("ERROR:activate_options:${1}")
 	fi
 } # activate_options
 
 color_formats () { # Print colorful terminal text
 	if [ "${activate_colors}" == 'yes' 2>/dev/null ]; then
-		whi=`tput setab 0; tput setaf 7` # Black background, white text
-		red=`tput setab 0; tput setaf 1` # Black background, red text
-		ora=`tput setab 0; tput setaf 3` # Black background, orange text
-		gre=`tput setab 0; tput setaf 2` # Black background, green text
-		blu=`tput setab 0; tput setaf 4` # Black background, blue text
-		pur=`tput setab 0; tput setaf 5` # Black background, purple text
-		formatreset=`tput sgr0`          # Reset to default terminal settings
+		whi=$(tput setab 0; tput setaf 7) # Black background, white text
+		red=$(tput setab 0; tput setaf 1) # Black background, red text
+		ora=$(tput setab 0; tput setaf 3) # Black background, orange text
+		gre=$(tput setab 0; tput setaf 2) # Black background, green text
+		blu=$(tput setab 0; tput setaf 4) # Black background, blue text
+		pur=$(tput setab 0; tput setaf 5) # Black background, purple text
+		formatreset=$(tput sgr0)          # Reset to default terminal settings
 	fi
 } # color_formats
 
-mac_readlink () { # Get absolute path of a file
-	input_path="${1}"
-	cwd="$(pwd)" # Current working directory path
+mac_readlink () { # Get absolute path of a file (mac and linux compatible)
+	dir_mac=$(dirname "${1}")   # Directory path
+	file_mac=$(basename "${1}") # Filename
+	wd_mac=$(pwd) # Working directory path
 
-	if [ -e "${input_path}" ]; then # if file or directory exists
-		cd "$(dirname ${input_path})"
-		abs_path="$(pwd)/$(basename ${input_path})"
-		cd "${cwd}" # Change directory back to original directory
-	fi
-
-	if [ -e "${abs_path}" ] && ! [ -z "${abs_path}" ]; then
-		echo "${abs_path}"
-	else # Invalid input or script error
-		echo "${input_path}" # echo original input
+	if [ -d "${dir_mac}" ]; then
+		cd "${dir_mac}"
+		echo "$(pwd)/${file_mac}" # Print full path
+		cd "${wd_mac}" # Change back to original directory
+	else
+		echo "${1}" # Print input
 	fi
 } # mac_readlink
 
-open_text_editor () { # Opens input file
-	file_to_open="${1}"
-	valid_text_editor='no'
+open_text_editor () { # Opens input file in background (GUI text editors only)
+	open_file="${1}"  # Input file
+	valid_editor='no' # Remains 'no' until command is valid
 	
-	if [ -f "${file_to_open}" ]; then
+	if [ -f "${open_file}" ]; then # If input file exists
 		for i in ${!text_editors[@]}; do # Loop through indices
-			${text_editors[i]} "${file_to_open}" 2>/dev/null &
+			eval "${text_editors[${i}]} ${open_file} 2>/dev/null &" # eval for complex commands
 			pid="$!" # Background process ID
-			check_text_pid=(`ps "${pid}" |grep "${pid}"`) # Check if pid is running
+			check_pid=($(ps "${pid}" |grep "${pid}")) # Check if pid is running
 			
-			if [ "${#check_text_pid[@]}" -gt '0' ] && [ "${check_text_pid[0]}" == "${pid}" 2>/dev/null ]; then
-				valid_text_editor='yes'
+			if [ "${#check_pid[@]}" -gt '0' ]; then
+				valid_editor='yes'
 				break
-			fi
+			fi # Break loop when valid command is found
 		done
 
-		if [ "${valid_text_editor}" == 'no' 2>/dev/null ]; then
-			echo "${red}NO VALID TEXT EDITORS: ${ora}${text_editors[@]}${whi}"
+		if [ "${valid_editor}" == 'no' 2>/dev/null ]; then
+			echo "${red}NO VALID TEXT EDITOR COMMANDS IN ${ora}text_editors ${red}ARRAY:${whi}"
+			printf "${ora}%s${IFS}${whi}" ${text_editors[@]}
 			exit_message 99
 		fi
-	else
-		echo "${red}MISSING FILE: ${ora}${file_to_open}${whi}"
-	fi
+	else # Missing input file
+		echo "${red}MISSING FILE: ${ora}${open_file}${whi}"
+	fi # if [ -f "${open_file}" ]; then
 } # open_text_editor
 
 backslash_file () { # Backslash characters '\' '$' '`' '"'
@@ -110,34 +130,6 @@ backslash_file () { # Backslash characters '\' '$' '`' '"'
 } # backslash_file
 
 #-------------------------------- MESSAGES ---------------------------------#
-script_usage () { # Script explanation
-	echo "${red}HELP MESSAGE: ${gre}${script_path}${whi}
-${ora}DESCRIPTION${whi}: Make 'create_script' function and copy into 'create_script.sh'
-     
-${ora}USAGE${whi}: Input template script and script number
- [${ora}1${whi}] ${gre}${script_path}${ora} ${HOME}/template.sh ${red}5${whi}
-     
-${ora}OPTIONS${whi}: Can input multiple options in any order
- ${pur}-h${whi} or ${pur}--help${whi} Display this message
- ${pur}-nc${whi}  Prevent color printing in terminal
- ${pur}-o${whi} or ${pur}--open${whi} Open this script
-     
-${ora}DEFAULT SETTINGS${whi}:
- script number: ${gre}${script_number}${whi}
- text editors : ${gre}${text_editors[@]}${whi}
-     
-${ora}VERSION: ${gre}${version_number}${whi}
-${red}END OF HELP: ${gre}${script_path}${whi}"
-	exit_message 0
-} # script_usage
-
-re_enter_input_message () { # Displays invalid input message
-	clear
-	echo "${red}INVALID INPUT: ${ora}"
-	printf '%s\n' ${@}
-	echo "${pur}PLEASE RE-ENTER INPUT${whi}"
-} # re_enter_input_message
-
 exit_message () { # Message before exiting script
 	if [ -z "${1}" 2>/dev/null ] || ! [ "${1}" -eq "${1}" 2>/dev/null ]; then
 		exit_type='0'
@@ -151,33 +143,39 @@ exit_message () { # Message before exiting script
 	
 	# Suggest help message
 	if [ "${suggest_help}" == 'yes' 2>/dev/null ]; then
-		echo "${ora}TO DISPLAY HELP MESSAGE TYPE: ${gre}${script_path} -h${whi}"
+		echo "${ora}FOR HELP: ${gre}${script_path} -h${whi}"
 	fi
 	
 	printf "${formatreset}\n"
 	exit "${exit_type}"
 } # exit_message
 
+invalid_msg () { # Displays invalid input message
+	clear
+	echo "${red}INVALID INPUT:${whi}"
+	printf "${ora}%s${IFS}${whi}" ${@}
+} # invalid_msg
+
 #---------------------------------- CODE -----------------------------------#
-script_path=$(mac_readlink "${script_path}") # equivalent to 'readlink -f' in linux
+script_path=$(mac_readlink "${script_path}") # similar to 'readlink -f' in linux
 
 for inputs; do # Reads through all inputs
 	option_eval "${inputs}"
 done
 
-color_formats # Activates or inhibits colorful output
+color_formats # Activate or prevent colorful output
 
-# Display help message or open file
+# Display help message or open script
 if [ "${activate_help}" == 'yes' ]; then # '-h' or '--help'
-	script_usage
+	usage # Display help message
 elif [ ${open_script} == 'yes' ]; then   # '-o' or '--open'
-	open_text_editor "${script_path}" ${text_editors[@]}
-	exit_message 0 -nm -nt
+	open_text_editor "${script_path}"
+	exit_message 0
 fi
 
 # Exit script if invalid inputs found
 if [ "${#bad_inputs[@]}" -gt '0' ]; then
-	re_enter_input_message ${bad_inputs[@]}
+	invalid_msg ${bad_inputs[@]}
 	exit_message 1
 fi
 
